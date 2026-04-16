658b3fa29e026b41e83607dda839f987c1d86993
Kauan Bruno <kauanbrunocs@gmail.com>
2026-04-16 14:53:17 -0300

    Modify SQL inserts for Cliente, Produto, and Pedido
    Updated customer, product, and order data with new entries and corrections.

--- Faculdade/T1.sql
+++ Faculdade/T1.sql
@@ -36,35 +36,52 @@ CREATE TABLE ItemPedido(
 INSERT INTO Cliente (Nome, Email, Cidade) VALUES 
 ('Matheus','math@gmail.com', 'Osasco'),
 ('Elie','elie@hotmail.com', 'Barueri'),
-('Kratos','bomdaguerra@outlook.com', 'Itapevi'),
+('Klaus','klaus@outlook.com', 'Itapevi'),
 ('Bruce','wayne@corp.com', 'Niterói'),
-('Clark','clarimdiario@gmail.com', 'São Paulo'),
-('Peter','starkfan@outlook.com','São Paulo'),
+('Clark','clarimd@gmail.com', 'São Paulo'),
+('Peter','parker@outlook.com','São Paulo'),
 ('Connor','tk800@cyberlife.com','Carapicuíba'),
 ('Arnold','schwarzenegger@hotmail.com','Cotia'),
-('Wile Coiote','wilec@acme.corp.br','Cotia'),
+('Wile','wilec@acme.corp.br','Cotia'),
 ('Leon','leonkennedy@gmail.com','Carapicuíba');
 
 INSERT INTO Produto (NomeProduto, Preco, Estoque) VALUES
-('Leviathan Axe', 35000.00,1),
-('Pólvora', 675.98,45),
-('Câmera Canon',4356.00,8),
-('TNT Acme', 899.99,100),
-('Manual Pai do Ano', 123.09,4),
-('Iron Man - Action Figure', 7600.90, 2),
-('Android T800', 456900.09,1),
-('Café 1KG', 167.90,8),
-('Esteroides Equinos', 768.90, 56),
-('Entendendo SQL', 876.98, 12);
+('PlayStation® 5 Slim', 3800.00,45),
+('PlayStation® 5 PRO', 6075.98,45),
+('PlayStation® 4',2556.00,8),
+('Alieware 16', 8999.99,100),
+('Monitor AOC 120Hz', 823.09,4),
+('Samsung S20+', 5600.90,2),
+('Teclado Redragon Kumara', 900.09,11),
+('Xbox One', 1767.90,8),
+('Headset Pulse Elite', 768.90,56),
+('Entendendo SQL', 126.98,12);
 
 INSERT INTO Pedido (IdCliente, DataPedido) VALUES
 (1, '2026-05-01'),
 (2, '2025-07-08'),
 (3, '2021-12-25'),
-(4, '1998-01-02'),
+(4, '2022-01-02'),
 (5, '2023-08-25'),
 (6, '2026-01-25'),
-(7, '2001-11-14'),
-(8, '1997-01-14'),
-(9, '2011-08-09'),
-(10,'2015-11-25');
+(7, '2020-11-14'),
+(8, '2020-01-14'),
+(9, '2025-08-09'),
+(10,'2025-11-25');
+
+INSERT INTO ItemPedido (IdPedido, IdProduto, Quantidade, ValorUnitario) VALUES
+(7, 8, 2, 1767.90),
+(7, 2, 1,6075.98),
+(8, 1, 2, 3800.00),
+(8, 2, 3, 6075.98),
+(3, 10, 2, 126.98),
+(4, 4, 2, 8999.99),
+(5, 3, 1, 2556.00),
+(2, 6, 2, 5600.92),
+(2, 4, 1, 8999.99),
+(2, 5, 1, 823.00),
+(9, 6, 1, 5600.00),
+(10, 10, 5, 126.98),
+(6, 7, 3, 900.90),
+(1, 1, 3, 6075.90),
+(1, 5, 1, 823.00);
